a5ce244e270c25980b41875473ef2cfc7eb9870d
jacobmarmstrong <44711734+jacobmarmstrong@users.noreply.github.com>
2018-11-05 08:42:11 -0600

    Update connection.php

--- include/connection.php
+++ include/connection.php
@@ -1,6 +1,6 @@
 <?php
 $mysqli = new mysqli("localhost","root","","claims");
 if ($mysqli->connect_errno) {
-    die("Failed to connect to the database: " . $mysqli->connect_errno);
+    die("Failed to connect to the database: " . $mysqli->connect_error);
 }
 ?>
